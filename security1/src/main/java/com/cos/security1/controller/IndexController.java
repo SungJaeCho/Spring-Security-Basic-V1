9510022d8745706160f38d942fe929b8b84a71b8
SungJaeCho <liadus@naver.com>
2021-05-15 00:06:30 +0900

    시큐리티 권한 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
    그 후 컨트롤러에서 @secured or @PreAuthorize를 통해 메소드 별로 권한처리가 가능하다.

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.cos.security1.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -62,5 +64,16 @@ public class IndexController {
 		return "redirect:/loginForm";
 	}
 	
+	@Secured("ROLE_ADMIN")
+	@GetMapping("/info")
+	public @ResponseBody String info() {
+		return "개인정보";
+	}
+	
+	@PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") // data() 직전에 실행됨
+	@GetMapping("/data")
+	public @ResponseBody String data() {
+		return "데이터정보";
+	}
 
 }
