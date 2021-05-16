bdfd44b386df148ba12782525effb524ac8c9b7b
SungJaeCho <liadus@naver.com>
2021-05-17 00:12:59 +0900

    구글 프로필정보 가져오기

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -8,10 +8,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+
+import lombok.RequiredArgsConstructor;
+
+// 1.코드받기(인증) 2.엑세스토큰(권한) 3.사용자프로필 정보 가져옴 4-1.그정보를 토대로 회원가입을 자동으로 진행
+// 4-2(이메일,전화번호,이름,아이디) 쇼핑몰 -> (집주소), 백화점몰 -> (Vip등급)
+
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	
+	private final PrincipalOauth2UserService principalOauth2UserService;
 	
 	// 해당 메서드의 리턴되는 오브젝트를 Ioc로 등록해줌
 	@Bean
@@ -34,7 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.defaultSuccessUrl("/")
 			.and()
 			.oauth2Login()
-			.loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함.
+			.loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드X, (엑세스토큰+사용자프로필정보 O)
+			.userInfoEndpoint()
+			.userService(principalOauth2UserService)
 			;
 	}
 	
